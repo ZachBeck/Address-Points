7be3dccd1f151f0ac3c36615540ad56e4dfe884a
Zach Beck <zbeck@utah.gov>
2024-09-30 14:01:29 -0600

    misc

--- testing.py
+++ testing.py
@@ -1,6 +1,5 @@
-from sweeper.address_parser import Address
+#from sweeper.address_parser import Address
 
-test_address = Address('4036 E North Fork Canyon Rd')
+# test_address = Address('4036 E North Fork Canyon Rd')
 
-print(test_address.prefix_direction)
-
+# print(test_address.prefix_direction)
